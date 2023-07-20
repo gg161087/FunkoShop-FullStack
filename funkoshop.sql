225c56566cae7cad0e70bd4d42e26b27a6b107e8
Gonzalo <gonzalo.gonzalez@sg.gba.gob.ar>
2023-07-20 11:26:40 -0300

    db funkoshop actualizada final

--- funkoshop.sql
+++ funkoshop.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 4.8.4
 -- https://www.phpmyadmin.net/
---
--- Servidor: localhost
--- Tiempo de generación: 13-07-2023 a las 23:49:29
--- Versión del servidor: 10.4.21-MariaDB
--- Versión de PHP: 7.4.29
+--
+-- Host: bllxj36u2qg6powvyu2e-mysql.services.clever-cloud.com:3306
+-- Generation Time: Jul 20, 2023 at 02:24 PM
+-- Server version: 8.0.15-5
+-- PHP Version: 7.2.34
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -18,13 +19,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `funkoshop`
+-- Database: `bllxj36u2qg6powvyu2e`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `category`
+-- Table structure for table `category`
 --
 
 CREATE TABLE `category` (
@@ -34,7 +35,7 @@ CREATE TABLE `category` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Volcado de datos para la tabla `category`
+-- Dumping data for table `category`
 --
 
 INSERT INTO `category` (`category_id`, `category_name`, `category_description`) VALUES
@@ -45,29 +46,30 @@ INSERT INTO `category` (`category_id`, `category_name`, `category_description`) 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `licence`
+-- Table structure for table `licence`
 --
 
 CREATE TABLE `licence` (
   `licence_id` int(11) NOT NULL,
   `licence_name` varchar(45) NOT NULL,
   `licence_description` varchar(255) NOT NULL,
-  `licence_image` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `licence_image` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 
 --
--- Volcado de datos para la tabla `licence`
+-- Dumping data for table `licence`
 --
 
 INSERT INTO `licence` (`licence_id`, `licence_name`, `licence_description`, `licence_image`) VALUES
-(1, 'Pokemon', 'Atrapa todos los que puedas y disfruta de una colección llena de amigos.', 'pokemon/pk-cover.jpg'),
-(2, 'Star Wars', 'Disfruta de una saga que sigue agregando personajes a su colección.', 'star-wars/st-cover.jpg'),
-(3, 'Harry Potter', 'Revive los recuerdos de una saga llena de magia y encanto.', 'harry-potter/hp-cover.jpg');
+(1, 'Pokemon', 'Atrapa todos los que puedas y disfruta de una colección llena de amigos.', 'pokemon/vulpix-1.webp'),
+(2, 'Star Wars', 'Disfruta de una saga que sigue agregando personajes a su colección.', 'star-wars/baby-yoda-1.webp'),
+(3, 'Harry Potter', 'Revive los recuerdos de una saga llena de magia y encanto.', 'harry-potter/snape-patronus-1.webp'),
+(4, 'Naruto', 'Disfruta de la historia de un ninja adolescente', 'naruto/nr-cover-png');
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `product`
+-- Table structure for table `product`
 --
 
 CREATE TABLE `product` (
@@ -81,33 +83,34 @@ CREATE TABLE `product` (
   `dues` int(11) DEFAULT NULL,
   `image_front` varchar(200) DEFAULT NULL,
   `image_back` varchar(200) DEFAULT NULL,
-  `create_time` timestamp NULL DEFAULT current_timestamp(),
+  `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `licence_id` int(11) NOT NULL,
   `category_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Volcado de datos para la tabla `product`
+-- Dumping data for table `product`
 --
 
-INSERT INTO `product` (`product_id`, `product_name`, `product_description`, `price`, `stock`, `discount`, `sku`, `dues`, `image_front`, `image_back`, `create_time`, `licence_id`, `category_id`) VALUES
-(1, 'Baby Yoda Blueball', 'Figura coleccionable de Baby Yoda (Grogu) - The Mandalorian Saga, edición limitada.', '1799.99', 8, 10, 'STW001001', 3, 'star-wars/baby-yoda-1.webp', 'star-wars/baby-yoda-box.webp', '2023-06-27 23:33:21', 2, 1),
-(3, 'Luke Skylwalker & Grogu', 'Figura coleccionable de Luke Skylwalker & Grogu - The Mandalorian Saga.', '2399.99', 8, 10, 'STW001003', 3, 'star-wars/luke-1.webp', 'star-wars/luke-box.webp', '2023-06-27 23:33:21', 2, 1),
-(4, 'Stormtrooper Lightsaber', 'Figura coleccionable de Stormtrooper Lightsaber - Star Wars Saga.', '1799.99', 8, 10, 'STW001004', 3, 'star-wars/trooper-1.webp', 'star-wars/trooper-box.webp', '2023-06-27 23:33:21', 2, 1),
-(5, 'Charmander Smiley', 'Figura coleccionable de Charmander - Pokemon Saga.', '1799.99', 8, 10, 'PKM001001', 3, 'pokemon/charmander-1.webp', 'pokemon/charmander-box.webp', '2023-06-27 23:33:21', 1, 1),
-(6, 'Dragonite Hi!', 'Figura coleccionable de Dragonite - Pokemon Saga.', '1799.99', 8, 10, 'PKM001002', 3, 'pokemon/dragonite-1.webp', 'pokemon/dragonite-box.webp', '2023-06-27 23:33:21', 1, 1),
-(7, 'Pidgeotto Flying', 'Figura coleccionable de Pidgeotto - Pokemon Saga.', '1799.99', 8, 10, 'PKM00103', 3, 'pokemon/pidgeotto-1.webp', 'pokemon/pidgeotto-box.webp', '2023-06-27 23:33:21', 1, 1),
-(8, 'Pikachu Smiley', 'Figura coleccionable de Pikachu - Pokemon Saga.', '1799.99', 8, 10, 'PKM001004', 3, 'pokemon/pikachu-1.webp', 'pokemon/pikachu-box.webp', '2023-06-27 23:33:21', 1, 1),
-(9, 'Vulpix Fancy', 'Figura coleccionable de Vulpix - Pokemon Saga.', '99.99', 8, 10, 'PKM001005', 3, 'pokemon/vulpix-1.webp', 'pokemon/vulpix-box.webp', '2023-06-27 23:33:21', 1, 1),
-(10, 'Harry Potter & Hegwid', 'Figura coleccionable de Harry Potter & Hegwid - Harry Potter Saga.', '1799.99', 11, 10, 'HPT0', 9, 'harry-potter/harry-1.webp', 'harry-potter/harry-box.webp', '2023-06-27 23:33:21', 3, 1),
-(11, 'Herminione Ball Dress', 'Figura coleccionable de Herminione - Harry Potter Saga.', '1799.99', 8, 10, 'HPT001002', 3, 'harry-potter/hermione-1.webp', 'harry-potter/hermione-box.webp', '2023-06-27 23:33:21', 3, 1),
-(12, 'Luna Lovegood Lion Mask', 'Figura coleccionable de Luna Lovegood - Harry Potter Saga.', '1799.99', 8, 10, 'HPT001003', 3, 'harry-potter/luna-1.webp', 'harry-potter/luna-box.webp', '2023-06-27 23:33:21', 3, 1),
-(13, 'Snape Patronus', 'Figura coleccionable de Snape Patronus - Harry Potter Saga.', '1799.99', 13, 10, 'HPT001004', 3, 'harry-potter/snape-patronus-1.webp', 'harry-potter/snape-patronus-box.webp', '2023-06-27 23:33:21', 3, 1);
+INSERT INTO `product` (`product_id`, `product_name`, `product_description`, `price`, `stock`, `discount`, `sku`, `dues`, `image_front`, `image_back`, `licence_id`, `category_id`) VALUES
+(1, 'Baby Yoda Blueball', 'Figura coleccionable de Baby Yoda (Grogu) - The Mandalorian Saga, edición limitada.', '1799.99', 8, 10, 'STW001001', 3, 'star-wars/baby-yoda-1.webp', 'star-wars/baby-yoda-box.webp', 2, 1),
+(2, 'Luke Skylwalker & Grogu', 'Figura coleccionable de Luke Skylwalker & Grogu - The Mandalorian Saga.', '2399.99', 8, 10, 'STW001003', 3, 'star-wars/luke-1.webp', 'star-wars/luke-box.webp', 2, 1),
+(3, 'Stormtrooper Lightsaber', 'Figura coleccionable de Stormtrooper Lightsaber - Star Wars Saga.', '1799.99', 8, 10, 'STW001004', 3, 'star-wars/trooper-1.webp', 'star-wars/trooper-box.webp', 2, 1),
+(4, 'Charmander Smiley', 'Figura coleccionable de Charmander - Pokemon Saga.', '1799.99', 8, 10, 'PKM001001', 3, 'pokemon/charmander-1.webp', 'pokemon/charmander-box.webp', 1, 1),
+(5, 'Dragonite Hi!', 'Figura coleccionable de Dragonite - Pokemon Saga.', '1799.99', 8, 10, 'PKM001002', 3, 'pokemon/dragonite-1.webp', 'pokemon/dragonite-box.webp', 1, 1),
+(6, 'Pidgeotto Flying', 'Figura coleccionable de Pidgeotto - Pokemon Saga.', '1799.99', 8, 10, 'PKM00103', 3, 'pokemon/pidgeotto-1.webp', 'pokemon/pidgeotto-box.webp', 1, 1),
+(7, 'Pikachu Smiley', 'Figura coleccionable de Pikachu - Pokemon Saga.', '1799.99', 8, 10, 'PKM001004', 3, 'pokemon/pikachu-1.webp', 'pokemon/pikachu-box.webp', 1, 1),
+(8, 'Vulpix Fancy', 'Figura coleccionable de Vulpix - Pokemon Saga.', '99.99', 8, 10, 'PKM001005', 3, 'pokemon/vulpix-1.webp', 'pokemon/vulpix-box.webp', 1, 1),
+(9, 'Harry Potter & Hegwid', 'Figura coleccionable de Harry Potter & Hegwid - Harry Potter Saga.', '1799.99', 11, 10, 'HPT001001', 9, 'harry-potter/harry-1.webp', 'harry-potter/harry-box.webp', 3, 1),
+(10, 'Herminione Ball Dress', 'Figura coleccionable de Herminione - Harry Potter Saga.', '1799.99', 8, 10, 'HPT001002', 3, 'harry-potter/hermione-1.webp', 'harry-potter/hermione-box.webp', 3, 1),
+(11, 'Luna Lovegood Lion Mask', 'Figura coleccionable de Luna Lovegood - Harry Potter Saga.', '1799.99', 8, 10, 'HPT001003', 3, 'harry-potter/luna-1.webp', 'harry-potter/luna-box.webp', 3, 1),
+(12, 'Snape Patronus', 'Figura coleccionable de Snape Patronus - Harry Potter Saga.', '1799.99', 13, 10, 'HPT001004', 3, 'harry-potter/snape-patronus-1.webp', 'harry-potter/snape-patronus-box.webp', 3, 1),
+(13, 'Kakashi Hatake Shippuden', 'Kakashi Hatake Shippuden', '1999.99', 20, 10, 'NRT001001', 9, '/naruto/1689698488200-kakashi-1.png', 'naruto/1689698488381-kakashi-box.png', 4, 1);
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `role`
+-- Table structure for table `role`
 --
 
 CREATE TABLE `role` (
@@ -118,7 +121,7 @@ CREATE TABLE `role` (
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `user`
+-- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
@@ -127,13 +130,13 @@ CREATE TABLE `user` (
   `lastname` varchar(80) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(32) NOT NULL,
-  `create_time` timestamp NULL DEFAULT current_timestamp()
+  `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `user_has_role`
+-- Table structure for table `user_has_role`
 --
 
 CREATE TABLE `user_has_role` (
@@ -142,23 +145,23 @@ CREATE TABLE `user_has_role` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Índices para tablas volcadas
+-- Indexes for dumped tables
 --
 
 --
--- Indices de la tabla `category`
+-- Indexes for table `category`
 --
 ALTER TABLE `category`
   ADD PRIMARY KEY (`category_id`);
 
 --
--- Indices de la tabla `licence`
+-- Indexes for table `licence`
 --
 ALTER TABLE `licence`
   ADD PRIMARY KEY (`licence_id`);
 
 --
--- Indices de la tabla `product`
+-- Indexes for table `product`
 --
 ALTER TABLE `product`
   ADD PRIMARY KEY (`product_id`),
@@ -167,19 +170,19 @@ ALTER TABLE `product`
   ADD KEY `fk_product_category1_idx` (`category_id`);
 
 --
--- Indices de la tabla `role`
+-- Indexes for table `role`
 --
 ALTER TABLE `role`
   ADD PRIMARY KEY (`role_id`);
 
 --
--- Indices de la tabla `user`
+-- Indexes for table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`user_id`);
 
 --
--- Indices de la tabla `user_has_role`
+-- Indexes for table `user_has_role`
 --
 ALTER TABLE `user_has_role`
   ADD PRIMARY KEY (`user_user_id`,`role_role_id`),
@@ -187,52 +190,52 @@ ALTER TABLE `user_has_role`
   ADD KEY `fk_user_has_role_user_idx` (`user_user_id`);
 
 --
--- AUTO_INCREMENT de las tablas volcadas
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT de la tabla `category`
+-- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
   MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT de la tabla `licence`
+-- AUTO_INCREMENT for table `licence`
 --
 ALTER TABLE `licence`
-  MODIFY `licence_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `licence_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT de la tabla `product`
+-- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 
 --
--- AUTO_INCREMENT de la tabla `role`
+-- AUTO_INCREMENT for table `role`
 --
 ALTER TABLE `role`
   MODIFY `role_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de la tabla `user`
+-- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
   MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- Restricciones para tablas volcadas
+-- Constraints for dumped tables
 --
 
 --
--- Filtros para la tabla `product`
+-- Constraints for table `product`
 --
 ALTER TABLE `product`
   ADD CONSTRAINT `fk_product_category1` FOREIGN KEY (`category_id`) REFERENCES `category` (`category_id`),
   ADD CONSTRAINT `fk_product_licence1` FOREIGN KEY (`licence_id`) REFERENCES `licence` (`licence_id`);
 
 --
--- Filtros para la tabla `user_has_role`
+-- Constraints for table `user_has_role`
 --
 ALTER TABLE `user_has_role`
   ADD CONSTRAINT `fk_user_has_role_role1` FOREIGN KEY (`role_role_id`) REFERENCES `role` (`role_id`),
